71852c9e56953e22a9367a21ce4a30362af0af93
Oleg Skiryuk <oskiryuk@beeline.ru>
2026-03-31 23:08:55 +0300

    Экспортировали иконки (кроме самих кривых)

---
+++ lib/core/themes/svg_assets.dart
@@ -0,0 +1,8 @@
+/// Пути к SVG-иконкам проекта.
+sealed class SvgAssets {
+  const SvgAssets._();
+
+  static const String icnHeart = 'assets/icons/icn_heart.svg';
+  static const String icnAdd = 'assets/icons/icn_add.svg';
+}
+
